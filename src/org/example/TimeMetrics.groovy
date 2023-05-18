cec7195204eb7c955c6f2ac3df279af0d1868591
jenasubhadev <jenasubhadev@gmail.com>
2023-05-18 19:25:13 +0530

    test

--- src/org/example/TimeMetrics.groovy
+++ src/org/example/TimeMetrics.groovy
@@ -14,4 +14,24 @@ class TimeMetrics implements Serializable {
 	def buildDockerImage(String imageName) {
 		script.sh "echo $imageName"
 	}
+	static Map<String, Long> stageTimes = [:]
+	static Map<String, Long> stepTimes = [:]
+  
+	static void calculateStageTime(String stageName, Closure closure) {
+	  def startTime = System.currentTimeMillis()
+	  closure.call()
+	  def endTime = System.currentTimeMillis()
+	  def duration = endTime - startTime
+	  stageTimes[stageName] = duration
+	  println "Stage '${stageName}' took ${duration} milliseconds"
+	}
+  
+	static void calculateStepTime(String stepName, Closure closure) {
+	  def startTime = System.currentTimeMillis()
+	  closure.call()
+	  def endTime = System.currentTimeMillis()
+	  def duration = endTime - startTime
+	  stepTimes[stepName] = duration
+	  println "Step '${stepName}' took ${duration} milliseconds"
+	}
 }
